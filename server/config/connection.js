1b362f54f822d4e3c675fb6fb9dd4b93c7f3fba9
Joseph A <joseph_anderson11@yahoo.com>
2022-12-12 16:54:04 -0500

    basic server connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,10 @@
-// Connect MongoDB and Server
+const { connect, connection } = require('mongoose');
+
+const connectionSTring = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bgcDB'
+
+connect(connectionSTring, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+module.exports = connection;
